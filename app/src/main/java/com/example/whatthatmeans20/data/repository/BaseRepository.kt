ad9952a4377b2988822a6c8b70c99c8c17742a93
Kshitij Kumar <kshitij2212@gmail.com>
2021-06-26 18:05:21 +0530

    http error code handled

--- app/src/main/java/com/example/whatthatmeans20/data/repository/BaseRepository.kt
+++ app/src/main/java/com/example/whatthatmeans20/data/repository/BaseRepository.kt
@@ -3,23 +3,29 @@ package com.example.whatthatmeans20.data.repository
 import com.example.whatthatmeans20.utils.Resources
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import org.json.JSONObject
 import retrofit2.Response
 import java.lang.Exception
 
 abstract class BaseRepository {
 
-    suspend fun <T>safeApi(call: suspend () -> Response<T>) : Resources<T> {
+    suspend fun <T> safeApi(call: suspend () -> Response<T>): Resources<T> {
         return withContext(Dispatchers.IO) {
             try {
                 val resp = call.invoke()
-                when(resp.code()) {
+                when (resp.code()) {
                     200 -> {
-                        if(resp.body() == null) Resources.Error("Call succesfull but empty response")
+                        if (resp.body() == null) Resources.Error("Call succesfull but empty response")
                         else Resources.Success(resp.body()!!)
                     }
-                    else -> Resources.Error("Something unexpected went wrong")
+                    else -> {
+                        val jsonObj =
+                            JSONObject(resp.errorBody()?.string() ?: "message:Something went wrong")
+                        Resources.Error(jsonObj.getString("message"))
+                    }
                 }
-            }catch (e: Exception) {
+            } catch (e: Exception) {
+                e.printStackTrace()
                 Resources.Error("Please check your internet connection!")
             }
         }
